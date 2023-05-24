a631ba6aefd80c8ac8b355280ae909b82d374a1d
shunjiezhao <939038111@qq.com>
2023-05-25 00:05:20 +0800

    feat(schedule):

--- scheduler/server/schedulers/balance_region.go
+++ scheduler/server/schedulers/balance_region.go
@@ -14,10 +14,12 @@
 package schedulers
 
 import (
+	"github.com/pingcap-incubator/tinykv/log"
 	"github.com/pingcap-incubator/tinykv/scheduler/server/core"
 	"github.com/pingcap-incubator/tinykv/scheduler/server/schedule"
 	"github.com/pingcap-incubator/tinykv/scheduler/server/schedule/operator"
 	"github.com/pingcap-incubator/tinykv/scheduler/server/schedule/opt"
+	"sort"
 )
 
 func init() {
@@ -77,6 +79,76 @@ func (s *balanceRegionScheduler) IsScheduleAllowed(cluster opt.Cluster) bool {
 
 func (s *balanceRegionScheduler) Schedule(cluster opt.Cluster) *operator.Operator {
 	// Your Code Here (3C).
-	panic("")
+
+	// bigRegion
+	var suitableStore []*core.StoreInfo
+	for _, store := range cluster.GetStores() {
+		if store.IsUp() && store.DownTime() < cluster.GetMaxStoreDownTime() {
+			suitableStore = append(suitableStore, store)
+		}
+	}
+	if len(suitableStore) == 0 {
+		log.Info("don't have any store")
+		return nil
+	}
+	if len(suitableStore) == 1 {
+		log.Info("only have one store")
+		return nil
+	}
+
+	sort.Slice(suitableStore, func(i, j int) bool {
+		return suitableStore[i].GetRegionSize() > suitableStore[j].GetRegionSize()
+	})
+	var bigRegion *core.RegionInfo
+	var bigStore, smallStore *core.StoreInfo
+
+	for _, store := range suitableStore {
+		type hander func(uint64, func(core.RegionsContainer))
+		for _, h := range []hander{cluster.GetPendingRegionsWithLock, cluster.GetFollowersWithLock, cluster.GetLeadersWithLock} {
+			h(store.GetID(), func(container core.RegionsContainer) {
+				region := container.RandomRegion(nil, nil)
+
+				if region != nil {
+					bigRegion = region
+					bigStore = store
+				}
+			})
+			if bigRegion != nil {
+				goto done
+			}
+		}
+	}
+
 	return nil
+done:
+	ids := bigRegion.GetStoreIds()
+	if len(ids) < cluster.GetMaxReplicas() {
+		log.Info("region don't have enough replicas")
+		return nil
+	}
+	for i := len(suitableStore) - 1; i >= 0; i-- {
+		if _, ok := ids[suitableStore[i].GetID()]; !ok {
+			if bigStore.GetRegionSize()-suitableStore[i].GetRegionSize() > 2*bigRegion.GetApproximateSize() {
+				smallStore = suitableStore[i]
+				break
+			}
+		}
+	}
+	if smallStore == nil {
+		log.Info("don't have suitable store")
+		return nil
+	}
+
+	peer, err := cluster.AllocPeer(smallStore.GetID())
+
+	if err != nil {
+		panic(err)
+	}
+	op, err := operator.CreateMovePeerOperator("balance-region", cluster, bigRegion, operator.OpBalance, bigStore.GetID(), smallStore.GetID(), peer.GetId())
+	if err != nil {
+		panic(err)
+	}
+
+	return op
+
 }
